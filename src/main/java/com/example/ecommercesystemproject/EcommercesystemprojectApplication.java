8754d8d83b5134c874631d6f112d700c05474d9d
wonyeolShin <lwy171400@gmail.com>
2026-07-10 20:30:28 +0900

    Initial Commit

---
+++ src/main/java/com/example/ecommercesystemproject/EcommercesystemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesystemprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercesystemprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercesystemproject/EcommercesystemprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercesystemprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
