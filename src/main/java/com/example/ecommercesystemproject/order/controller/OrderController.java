d41574db635ceff547151769905c4b0cfe051454
junyho <inop159@naver.com>
2026-07-11 16:25:47 +0900

    feat: Order domain 기본 구조 추가

---
+++ src/main/java/com/example/ecommercesystemproject/order/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.example.ecommercesystemproject.order.controller;
+
+import com.example.ecommercesystemproject.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+    private final OrderService orderService;
+
+}
---
+++ src/main/java/com/example/ecommercesystemproject/order/entity/Order.java
@@ -0,0 +1,19 @@
+package com.example.ecommercesystemproject.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
