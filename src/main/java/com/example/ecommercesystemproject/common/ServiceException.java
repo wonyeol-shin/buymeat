ab12cc85b6957568bbd2e40d758e5598ad803c34
wonyeolShin <lwy171400@gmail.com>
2026-07-11 18:38:41 +0900

    fix: 공통 클래스 내부 구현부 내용 추가

--- src/main/java/com/example/ecommercesystemproject/common/ServiceException.java
+++ src/main/java/com/example/ecommercesystemproject/common/ServiceException.java
@@ -1,7 +1,18 @@
 package com.example.ecommercesystemproject.common;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+// 수정하지 마시고 상속 받아서 자유롭게 예외 처리 하십쇼
+// 공통 에러 처리 클래스
+@Getter
 public class ServiceException extends RuntimeException {
-  public ServiceException(String message) {
-    super(message);
-  }
+
+    private final HttpStatus status;
+
+    public ServiceException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
 }
