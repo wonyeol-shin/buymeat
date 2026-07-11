67c609a742060fc1a1d965c887c55f2727ecbc54
junyho <inop159@naver.com>
2026-07-11 18:55:57 +0900

    refactor: Order 엔티티에 BaseEntity 상속 적용

--- src/main/java/com/example/ecommercesystemproject/order/entity/Order.java
+++ src/main/java/com/example/ecommercesystemproject/order/entity/Order.java
@@ -1,5 +1,6 @@
 package com.example.ecommercesystemproject.order.entity;
 
+import com.example.ecommercesystemproject.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Order {
+public class Order extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
