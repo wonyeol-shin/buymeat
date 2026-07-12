04bb5253decf11cff96e8be2c21918d803830478
momo <ptjkjm1@gmail.com>
2026-07-12 17:32:10 +0900

    refactor: Review 엔티티 불필요한 import 제거

--- src/main/java/com/example/ecommercesystemproject/review/entity/Review.java
+++ src/main/java/com/example/ecommercesystemproject/review/entity/Review.java
@@ -8,15 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
