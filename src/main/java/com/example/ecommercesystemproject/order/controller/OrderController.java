21fe3c866df663ed55107cb21d625ecb90717778
junyho <inop159@naver.com>
2026-07-13 10:50:38 +0900

    fix: 주문 API 경로에 /api 접두사 추가

--- src/main/java/com/example/ecommercesystemproject/order/controller/OrderController.java
+++ src/main/java/com/example/ecommercesystemproject/order/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/order")
+@RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
 
