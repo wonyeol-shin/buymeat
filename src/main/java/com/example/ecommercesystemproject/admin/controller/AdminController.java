f76310734ca2bfc02dba7cf94d61f3be90611f46
wonyeolShin <lwy171400@gmail.com>
2026-07-13 15:23:09 +0900

    최종 push 목적

--- src/main/java/com/example/ecommercesystemproject/admin/controller/AdminController.java
+++ src/main/java/com/example/ecommercesystemproject/admin/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RequestMapping
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
