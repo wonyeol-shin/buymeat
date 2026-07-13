854373213650b446d9fa2245500023e77630dd0e
junyho <inop159@naver.com>
2026-07-13 10:54:32 +0900

    feat: 주문 생성 기능 추가

--- src/main/java/com/example/ecommercesystemproject/order/controller/OrderController.java
+++ src/main/java/com/example/ecommercesystemproject/order/controller/OrderController.java
@@ -1,7 +1,13 @@
 package com.example.ecommercesystemproject.order.controller;
 
+import com.example.ecommercesystemproject.order.dto.CreateOrderRequest;
+import com.example.ecommercesystemproject.order.dto.CreateOrderResponse;
 import com.example.ecommercesystemproject.order.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
+
+    public ResponseEntity<CreateOrderResponse> create(@Valid @RequestBody CreateOrderRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.create(request));
+    }
+
 
 }
--- src/main/java/com/example/ecommercesystemproject/order/service/OrderService.java
+++ src/main/java/com/example/ecommercesystemproject/order/service/OrderService.java
@@ -1,6 +1,11 @@
 package com.example.ecommercesystemproject.order.service;
 
+import com.example.ecommercesystemproject.admin.entity.Role;
+import com.example.ecommercesystemproject.order.dto.CreateOrderRequest;
+import com.example.ecommercesystemproject.order.dto.CreateOrderResponse;
+import com.example.ecommercesystemproject.order.entity.Order;
 import com.example.ecommercesystemproject.order.repository.OrderRepository;
+import com.example.ecommercesystemproject.order.util.OrderNumberGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +16,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
     private final OrderRepository orderRepository;
 
+    @Transactional
+    public CreateOrderResponse create(CreateOrderRequest request) {
+
+        // long totalPrice = (long) product.getPrice() * request.getQuantity();
+        Order order = new Order(
+                request.getQuantity(),
+                OrderNumberGenerator.generate(),
+                100 //임시값
+                );
+        Order savedOrder = orderRepository.save(order);
+
+        // 임시값 수정 해야함
+        return new CreateOrderResponse(
+                savedOrder.getId(),
+                savedOrder.getOrderNumber(),
+                "customer",
+                "product",
+                savedOrder.getQuantity(),
+                savedOrder.getCreatedAt().toLocalDate(),
+                savedOrder.getStatus(),
+                savedOrder.getTotalPrice(),
+                "customerEmail",
+                1L,
+                "aminName",
+                "adminEmail",
+                Role.OP
+        );
+    }
 }
