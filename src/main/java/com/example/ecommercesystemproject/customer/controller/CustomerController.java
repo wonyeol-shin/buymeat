95868aadf515f5bbabf7c66e0a896667ea857f63
junyho <inop159@naver.com>
2026-07-14 14:40:02 +0900

    fix: 공통 응답처리

--- src/main/java/com/example/ecommercesystemproject/customer/controller/CustomerController.java
+++ src/main/java/com/example/ecommercesystemproject/customer/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.ecommercesystemproject.customer.controller;
 
+import com.example.ecommercesystemproject.common.response.ApiResponse;
 import com.example.ecommercesystemproject.customer.dto.*;
 import com.example.ecommercesystemproject.customer.service.CustomerService;
 import jakarta.validation.Valid;
@@ -21,45 +22,80 @@ public class CustomerController {
 
     // 고객 생성
     @PostMapping
-    public ResponseEntity<CreateCustomerResponse> createCustomer(
+    public ResponseEntity<ApiResponse<CreateCustomerResponse>> createCustomer(
             @Valid @RequestBody CreateCustomerRequest request
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(request));
+        CreateCustomerResponse response = customerService.createCustomer(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.of(
+                        HttpStatus.CREATED.value(),
+                        "고객 등록 성공",
+                        response
+                )
+        );
     }
 
     // 고객 전체 조회(+ 조건 검색)
     @GetMapping
-    public ResponseEntity<Page<GetCustomerResponse>> getAll(
+    public ResponseEntity<ApiResponse<Page<GetCustomerResponse>>> getAll(
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             CustomerSearchCondition condition
     ) {
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.getAllCustomer(pageable, condition));
+        Page<GetCustomerResponse> responses = customerService.getAllCustomer(pageable, condition);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "고객 목록 조회 성공",
+                        responses
+                )
+        );
     }
 
     // 고객 단건 조회
     @GetMapping("/{customerId}")
-    public ResponseEntity<GetCustomerResponse> getOne(
+    public ResponseEntity<ApiResponse<GetCustomerResponse>> getOne(
             @PathVariable Long customerId
     ) {
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.getOneCustomer(customerId));
+        GetCustomerResponse response = customerService.getOneCustomer(customerId);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "고객 상세 조회 성공",
+                        response
+                )
+        );
     }
 
     // 고객 정보 수정
     @PutMapping("/{customerId}")
-    public ResponseEntity<UpdateCustomerResponse> updateCustomer(
+    public ResponseEntity<ApiResponse<UpdateCustomerResponse>> updateCustomer(
             @PathVariable Long customerId,
             @RequestBody UpdateCustomerRequest request
     ) {
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.updateCustomer(customerId, request));
+        UpdateCustomerResponse response = customerService.updateCustomer(customerId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "고객 정보 수정 성공",
+                        response
+                )
+        );
     }
 
     // 고객 상태 변경
     @PatchMapping("/{customerId}/status")
-    public ResponseEntity<UpdateCustomerStatusResponse> updateCustomerStatus(
+    public ResponseEntity<ApiResponse<UpdateCustomerStatusResponse>> updateCustomerStatus(
             @PathVariable Long customerId,
             @Valid @RequestBody UpdateCustomerStatusRequest request
-            ) {
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.updateCustomerStatus(customerId, request));
+    ) {
+        UpdateCustomerStatusResponse response = customerService.updateCustomerStatus(customerId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "고객 상태 변경 성공",
+                        response
+                )
+        );
     }
 
     // 고객 삭제
--- src/main/java/com/example/ecommercesystemproject/product/controller/ProductController.java
+++ src/main/java/com/example/ecommercesystemproject/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.ecommercesystemproject.product.controller;
 
 import com.example.ecommercesystemproject.admin.dto.AdminSession;
 import com.example.ecommercesystemproject.common.constant.SessionConst;
+import com.example.ecommercesystemproject.common.response.ApiResponse;
 import com.example.ecommercesystemproject.product.dto.*;
 import com.example.ecommercesystemproject.product.service.ProductService;
 import jakarta.validation.Valid;
@@ -17,63 +18,107 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/products")
-    public ResponseEntity<CreateProductResponse> create
+    public ResponseEntity<ApiResponse<CreateProductResponse>> create
             (@Valid @RequestBody CreateProductRequest request,
-            @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(productService.createProduct(request, longAdminId)); // 201
+        CreateProductResponse response = productService.createProduct(request, longAdminId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.of(
+                        HttpStatus.CREATED.value(),
+                        "상품 등록 성공",
+                        response)
+        ); // 201
     }
 
     @GetMapping("/api/products")
-    public ResponseEntity<List<GetProductsResponse>> getAll
-            (@SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+    public ResponseEntity<ApiResponse<List<GetProductsResponse>>> getAll
+            (@SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.ok(productService.getAllProducts(longAdminId)); // 200
+        List<GetProductsResponse> responses = productService.getAllProducts(longAdminId);
+        return ResponseEntity.ok(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "상품 목록 조회 성공",
+                        responses
+                )
+        ); // 200
     }
 
     @GetMapping("/api/products/{productId}")
-    public ResponseEntity<GetProductResponse> getOne
+    public ResponseEntity<ApiResponse<GetProductResponse>> getOne
             (@PathVariable Long productId,
-             @SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.ok(productService.getOneProduct(productId, longAdminId));
+        GetProductResponse response = productService.getOneProduct(productId, longAdminId);
+
+        return ResponseEntity.ok(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "상품 상세 조회 성공",
+                        response
+                )
+        );
     }
 
     @PutMapping("/api/products/{productId}")
-    public ResponseEntity<UpdateProductResponse> update
+    public ResponseEntity<ApiResponse<UpdateProductResponse>> update
             (@PathVariable Long productId, @Valid @RequestBody UpdateProductRequest request,
-             @SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.ok(productService.updateProduct(productId, request, longAdminId));
+        UpdateProductResponse response = productService.updateProduct(productId, request, longAdminId);
+
+        return ResponseEntity.ok(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "상품 수정 성공",
+                        response
+                )
+        );
     }
 
     @PatchMapping("/api/products/{productId}/stock")
-    public ResponseEntity<UpdateProductStockResponse> updateStock
+    public ResponseEntity<ApiResponse<UpdateProductStockResponse>> updateStock
             (@PathVariable Long productId, @Valid @RequestBody UpdateProductStockRequest request,
-             @SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.ok(productService.updateProductStock(productId, request, longAdminId));
+        UpdateProductStockResponse response = productService.updateProductStock(productId, request, longAdminId);
+
+        return ResponseEntity.ok(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "상품 재고 수정 성공",
+                        response
+                )
+        );
     }
 
     @PatchMapping("/api/products/{productId}/status")
-    public ResponseEntity<UpdateProductStatusResponse> updateStatus
+    public ResponseEntity<ApiResponse<UpdateProductStatusResponse>> updateStatus
             (@PathVariable Long productId, @Valid @RequestBody UpdateProductStatusRequest request,
-             @SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
-        return ResponseEntity.ok(productService.updateProductStatus(productId, request, longAdminId));
+        UpdateProductStatusResponse response = productService.updateProductStatus(productId, request, longAdminId);
+
+        return ResponseEntity.ok(
+                ApiResponse.of(
+                        HttpStatus.OK.value(),
+                        "상품 상태 수정 성공",
+                        response
+                )
+        );
     }
 
     @DeleteMapping("/api/products/{productId}")
     public ResponseEntity<Void> delete
             (@PathVariable Long productId,
-             @SessionAttribute(name = "loginAdmin", required = false) AdminSession loginAdmin) {
+             @SessionAttribute(name = SessionConst.LOGIN_ADMIN_ID, required = false) AdminSession loginAdmin) {
 
         Long longAdminId = loginAdmin == null ? null : loginAdmin.getId();
         productService.deleteProduct(productId, longAdminId);
