056246108333accff19b467133ccc84d690e975a
junyho <inop159@naver.com>
2026-07-14 20:16:29 +0900

    fix: keyword null 처리

--- src/main/java/com/example/ecommercesystemproject/review/service/ReviewService.java
+++ src/main/java/com/example/ecommercesystemproject/review/service/ReviewService.java
@@ -20,7 +20,11 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
 
     public List<ListReviewResponse> getAllReview(Pageable pageable, String keyword) {
-        return reviewRepository.findAllWithCustomerAndOrder(pageable, keyword.trim()).stream()
+        return reviewRepository.findAllWithCustomerAndOrder(
+                        pageable,
+                        keyword == null ? null : keyword.trim()
+                )
+                .stream()
                 .map(ListReviewResponse::from)
                 .toList();
     }
