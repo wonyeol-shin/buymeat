e462f348e1ad2fe318d0315a611efbaa82a70b00
wonyeolShin <lwy171400@gmail.com>
2026-07-15 13:49:19 +0900

    admin service bug fix

--- src/main/java/com/example/ecommercesystemproject/admin/dto/UpdateMyPasswordRequest.java
+++ src/main/java/com/example/ecommercesystemproject/admin/dto/UpdateMyPasswordRequest.java
@@ -2,6 +2,7 @@ package com.example.ecommercesystemproject.admin.dto;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class UpdateMyPasswordRequest {
     @Size(min = 8, max = 255, message = "비밀번호는 최소 8글자 이상 최대 255글자까지 입력 가능합니다." )
     private String checkPassword;
 
+    @JsonIgnore // 💡 "이 메서드는 JSON으로 변환하거나 유추할 때 없는 셈 쳐라!" 라는 뜻
     public boolean isDifferentPassword() {
         return !this.newPassword.equals(this.checkPassword);
     }
